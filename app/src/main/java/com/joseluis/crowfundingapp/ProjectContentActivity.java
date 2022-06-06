e76e0045969e27fd2fc3c1ebad35fe0fa8da7bb5
canarionjl <canarionjl@gmail.com>
2022-06-06 19:01:49 +0100

    Diseño inicial de layouts realizado

---
+++ app/src/main/java/com/joseluis/crowfundingapp/ProjectContentActivity.java
@@ -0,0 +1,12 @@
+package com.joseluis.crowfundingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
